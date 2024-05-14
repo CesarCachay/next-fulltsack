f7d3050dd5088a542f4fdb9dc90481c7d12dbc7f
Briya <cesarcachay1996@gmail.com>
2024-05-14 18:53:51 -0500

    Add first server action to create a new invoice with POST request

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,16 +1,26 @@
-import { CustomerField } from '@/app/lib/definitions';
+// vendors
 import Link from 'next/link';
+
+// components
+import { Button } from '@/app/ui/button';
+
+// icons
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+
+// utils
+import { createInvoice } from '@/app/lib/actions';
+
+// types
+import { CustomerField } from '@/app/lib/definitions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
